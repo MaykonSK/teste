85cc231c8f683e2ada2a1fd0b68e705c2f73e49f
Maycon <mayconrodrigo123@hotmail.com>
2022-01-09 16:22:05 -0300

    Mensagem de ola mundo inserido

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,6 +4,7 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Versão 1");
+            Console.WriteLine("Olá mundo");
         }
     }
 }
